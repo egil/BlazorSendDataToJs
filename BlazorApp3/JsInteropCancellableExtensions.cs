ecb25eac732e92b792d030436e7bf59d7448bb34
Egil Hansen <egil@assimilated.dk>
2024-02-27 09:42:40 +0000

    experiments generic wrapper

---
+++ BlazorApp3/JsInteropCancellableExtensions.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorApp3;
+
+public static class JsInteropCancellableExtensions
+{
+    public static async ValueTask InvokeStreamVoidAsync(this IJSRuntime js, string identifier, CancellationToken cancellationToken, Stream stream, params object?[]? args)
+    {
+        using var wrappedStream = new BlazorCancellableStreamWrapper(stream, cancellationToken);
+        using var streamRef = new DotNetStreamReference(wrappedStream);
+        var streamIdentifier = cancellationToken.GetHashCode() + identifier;
+
+        cancellationToken.Register(static (state) =>
+        {
+            var (js, identifier) = ((IJSRuntime, string))state!;
+            js.InvokeVoidAsync("BlazorStreamingJsInterop.cancelInvokeAsync", identifier);
+        }, state: (js, streamIdentifier));
+
+        args = [streamIdentifier, streamRef, identifier, .. args];
+
+        await js.InvokeVoidAsync(
+            "BlazorStreamingJsInterop.invokeAsyncWithCancellation",
+            cancellationToken: cancellationToken,
+            args: args);
+    }
+
+    private sealed class BlazorCancellableStreamWrapper : Stream
+    {
+        private readonly Stream innerStream;
+        private readonly CancellationToken streamCancellationToken;
+
+        public BlazorCancellableStreamWrapper(Stream innerStream, CancellationToken streamCancellationToken)
+        {
+            this.innerStream = innerStream;
+            this.streamCancellationToken = streamCancellationToken;
+        }
+
+        public override ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            // Because SendDotNetStreamToJS (https://source.dot.net/#Microsoft.AspNetCore.Components.Server/ComponentHub.cs,287)
+            // will throw if the stream gets disposed before it is finished sending the full content,
+            // this effectively stops it from continuing after the cancellation token has been set. We cheat
+            // it to think the stream is done and DotNetStreamReference will then dispose it.
+            if (streamCancellationToken.IsCancellationRequested)
+            {
+                return ValueTask.FromResult(0);
+            }
+
+            return innerStream.ReadAsync(buffer, cancellationToken);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                innerStream.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        public override ValueTask DisposeAsync()
+        {
+            innerStream.DisposeAsync();
+            return base.DisposeAsync();
+        }
+
+        public override bool CanRead => throw new NotImplementedException();
+
+        public override bool CanSeek => throw new NotImplementedException();
+
+        public override bool CanWrite => throw new NotImplementedException();
+
+        public override long Length => throw new NotImplementedException();
+
+        public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BlazorApp3/wwwroot/app.js
+++ BlazorApp3/wwwroot/app.js
@@ -65,8 +65,12 @@ window.loadCompressedData = function (chart, data, layout) {
     });
 }
 
-window.invokeMe = function (name, randomPayload) {
-    console.log(name + " | " + randomPayload.length + " | " + new Date().toISOString());
+window.invokeMe = function (name, randomPayload, additional) {
+    console.log(`${name} | ${randomPayload.x.length} | ${new Date().toISOString()} | ${additional}`);
+}
+
+window.invokeMeX = function (randomPayload, additional) {
+    console.log(`${randomPayload[0].x.length} | ${new Date().toISOString()} | ${additional}`);
 }
 
 const aborters = {
@@ -103,3 +107,55 @@ async function processJsonStream(abortSignal, stream) {
     const decompressedStream = stream.pipeThrough(new DecompressionStream('gzip'), { signal: abortSignal });
     return new Response(decompressedStream).json();
 }
+
+class BlazorStreamingJsInterop {
+    static abortControllers = new Map();
+
+    static async invokeAsyncWithCancellation(cancellationTokenId, dotNetStreamRef, identifier, args) {
+        const abortController = new AbortController();
+        this.abortControllers[cancellationTokenId] = abortController;
+
+        let stream = await dotNetStreamRef.stream();
+        stream = stream.pipeThrough(new TransformStream({
+            transform(chunk, controller) {
+                if (abortController.signal.aborted) {
+                    controller.terminate();
+                    return;
+                }
+                controller.enqueue(chunk);
+            }
+        }));
+
+        try {
+            const json = await new Response(stream).json();
+
+            if (!abortController.signal.aborted) {
+                delete this.abortControllers[cancellationTokenId];
+                const jsFunction = DotNet.findJSFunction(identifier, 0);
+                let argsArray = [];
+                if (Array.isArray(args)) {
+                    argsArray = args;
+                } else if (args !== undefined) {
+                    argsArray = [args];
+                }
+                argsArray = [json, ...argsArray];
+                jsFunction.apply(null, argsArray);
+            }
+        }
+        catch (err) {
+            if (!abortController.signal.aborted) {
+                throw err;
+            }
+        }
+    }
+
+    static cancelInvokeAsync(cancellationTokenId) {
+        if (this.abortControllers[cancellationTokenId]) {
+            console.log(`Cancelling invokeAsyncWithCancellation with id ${cancellationTokenId}`);
+            this.abortControllers[cancellationTokenId].abort('Blazor JS streaming call cancelled by server.');
+            delete this.abortControllers[cancellationTokenId];
+        }
+    }
+}
+
+window.BlazorStreamingJsInterop = BlazorStreamingJsInterop;
